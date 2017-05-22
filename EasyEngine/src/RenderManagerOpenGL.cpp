61386231b5d1217088497c841139d5f71569408f
Calle Bergström <calle.bergstrom@live.se>
2017-05-22 20:01:04 +0200

    Added depth testing. Added version debug message

--- EasyEngine/src/RenderManagerOpenGL.cpp
+++ EasyEngine/src/RenderManagerOpenGL.cpp
@@ -28,6 +28,15 @@ namespace easy_engine {
 			glewExperimental = GL_TRUE;
 			glewInit();
 
+			const GLubyte* renderer = glGetString(GL_RENDERER);
+			const GLubyte* version = glGetString(GL_VERSION);
+			std::cout << "Renderer: " << renderer << std::endl;
+			std::cout << "OpenGL version supported: " << version << std::endl;
+
+			// tell GL to only draw onto a pixel if the shape is closer to the viewer
+			glEnable(GL_DEPTH_TEST); // enable depth-testing
+			glDepthFunc(GL_LESS); // depth-testing interprets a smaller value as "closer"
+
 			this->LoadShaders();
 		};
 
@@ -37,8 +46,8 @@ namespace easy_engine {
 
 		void RenderManagerOpenGL::Render() {
 
-			// Clear the screen to black
-			glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+			// Set background to dark grey
+			glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
 
 			float test_triangle_[9] = {
 				-0.5f, -0.5f, 0.0f,
@@ -58,14 +67,18 @@ namespace easy_engine {
 			// Setup vertex array object for triangles
 			glBindVertexArray(this->vao_[0]);
 
+			// Set active VBO to position VBO
 			glBindBuffer(GL_ARRAY_BUFFER, this->vbo_[0]);
 			std::cout << "Triangle position VBO id: " << this->vbo_[0] << std::endl;
+			// Upload data to VBO
 			glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), test_triangle_, GL_STATIC_DRAW);
 			glEnableVertexAttribArray(0);
 			glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
+			// Set active VBO to color VBO
 			glBindBuffer(GL_ARRAY_BUFFER, this->vbo_[1]);
 			std::cout << "Triangle color VBO id: " << this->vbo_[1] << std::endl;
+			// Upload data to VBO
 			glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), test_triangle_color, GL_STATIC_DRAW);
 			glEnableVertexAttribArray(1);
 			glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 0, 0);
@@ -76,8 +89,30 @@ namespace easy_engine {
 
 				if (glfwGetKey(this->window_, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 					glfwSetWindowShouldClose(this->window_, GL_TRUE);
+				else if(glfwGetKey(this->window_, GLFW_KEY_A) == GLFW_PRESS) {
+					test_triangle_[0] -= 0.002f;
+					test_triangle_[3] -= 0.002f;
+					test_triangle_[6] -= 0.002f;
+				} else if(glfwGetKey(this->window_, GLFW_KEY_D) == GLFW_PRESS) {
+					test_triangle_[0] += 0.002f;
+					test_triangle_[3] += 0.002f;
+					test_triangle_[6] += 0.002f;
+				} else if (glfwGetKey(this->window_, GLFW_KEY_W) == GLFW_PRESS) {
+					test_triangle_[1] += 0.002f;
+					test_triangle_[4] += 0.002f;
+					test_triangle_[7] += 0.002f;
+				} else if (glfwGetKey(this->window_, GLFW_KEY_S) == GLFW_PRESS) {
+					test_triangle_[1] -= 0.002f;
+					test_triangle_[4] -= 0.002f;
+					test_triangle_[7] -= 0.002f;
+				}
 
-				glClear(GL_COLOR_BUFFER_BIT);
+				glBindBuffer(GL_ARRAY_BUFFER, this->vbo_[0]);
+				glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), test_triangle_, GL_STATIC_DRAW);
+				glEnableVertexAttribArray(0);
+				glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
+
+				glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 				// Activate the triangle vertex array object
 				glBindVertexArray(this->vao_[0]);
